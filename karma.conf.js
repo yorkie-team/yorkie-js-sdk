2f0db1d8de5469f381410c6b8b727e0ee98a9b0f
Semin Park <SPark9625@gmail.com>
2020-08-21 19:33:08 +0900

    Add sourcemap to karma

--- karma.conf.js
+++ karma.conf.js
@@ -37,8 +37,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/**/*.ts': ['webpack'],
-      'test/**/*.js': ['webpack']
+      'test/**/*.ts': ['webpack', 'sourcemap'],
+      'test/**/*.js': ['webpack', 'sourcemap']
     },
 
 
