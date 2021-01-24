3a92afe99cb80b70bd95f0b5897a24239dd3a5d4
dc7303 <dc7303@gmail.com>
2021-01-24 11:38:40 +0900

    Add no-unreachable option in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'tsdoc/syntax': 'error',
     'object-shorthand': ['error', 'always'],
+    'no-unreachable': 'error',
   },
 };
