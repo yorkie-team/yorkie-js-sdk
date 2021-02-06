5e64b77133be68da68fb584fa085becdb10f14cf
hackerwins <susukang98@gmail.com>
2021-02-06 20:24:31 +0900

    Export ActorID

--- src/yorkie.ts
+++ src/yorkie.ts
@@ -19,6 +19,7 @@ import { Client, ClientOptions } from './core/client';
 import { Document } from './document/document';
 
 export { Client, Document };
+export { ActorID } from './document/time/actor_id';
 export { JSONElement } from './document/json/element';
 export { JSONObject } from './document/json/object';
 export { JSONArray } from './document/json/array';
