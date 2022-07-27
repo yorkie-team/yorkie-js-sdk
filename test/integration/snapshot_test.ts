851359b95b4e3b2b952e19505f637eb15e6f268e
Eithea <oguraff94@gmail.com>
2022-07-27 19:00:17 +0900

    add test

--- test/integration/snapshot_test.ts
+++ test/integration/snapshot_test.ts
@@ -1,5 +1,6 @@
 import { assert } from 'chai';
 import { withTwoClientsAndDocuments } from '@yorkie-js-sdk/test/integration/integration_helper';
+import { Text } from '@yorkie-js-sdk/src/yorkie';
 
 describe('Snapshot', function () {
   it('should handle snapshot', async function () {
@@ -23,6 +24,37 @@ describe('Snapshot', function () {
       await c1.sync();
       await c2.sync();
       await c1.sync();
+      assert.equal(d1.toSortedJSON(), d2.toSortedJSON());
+    }, this.test!.title);
+  });
+
+  it('should handle snapshot for text object', async function () {
+    await withTwoClientsAndDocuments<{ k1: Text }>(async (c1, d1, c2, d2) => {
+      
+      for (let idx = 0; idx < 700; idx++) {
+        d1.update((root) => {
+          root.k1 = new Text();
+        }, 'set new doc by c1');
+      }
+      await c1.sync();
+      await c2.sync();
+
+      // 01. Updates 700 changes over snapshot threshold by c1.
+      for (let idx = 0; idx < 700; idx++) {
+        d1.update((root) => {
+          root.k1.edit(idx, idx, 'x');
+        });
+      }
+
+      // 02. Makes local change by c2.
+      d2.update((root) => {
+        root.k1.edit(0, 0, 'o');
+      });
+
+      await c1.sync();
+      await c2.sync();
+      await c1.sync();
+
       assert.equal(d1.toSortedJSON(), d2.toSortedJSON());
     }, this.test!.title);
   });
