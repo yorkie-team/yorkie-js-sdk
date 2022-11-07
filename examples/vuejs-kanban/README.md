f7bf6d93dc7ec65cb4586b476c6669ea20919807
Youngteac Hong <susukang98@gmail.com>
2022-11-07 21:18:45 +0900

    Add vuejs-kanban example (#399)

---
+++ examples/vuejs-kanban/README.md
@@ -0,0 +1,21 @@
+# Yorkie Vue Kanban Example
+
+## How to run demo
+
+At project root, run below command to start Yorkie and Envoy proxy.
+
+```bash
+$ docker-compose -f docker/docker-compose.yml up --build -d
+```
+
+Install dependencies
+
+```bash
+$ npm install
+```
+
+Start demo project
+
+```bash
+$ npm run dev
+```
---
+++ examples/vuejs-kanban/src/App.vue
@@ -0,0 +1,165 @@
+<script>
+import yorkie from 'yorkie-js-sdk';
+
+const defaultLists = [
+  {
+    title: 'Todo',
+    cards: [
+      {
+        title: 'Pruning document',
+      },
+      {
+        title: 'Clean up codes'
+      }
+    ]
+  },
+  {
+    title: 'Doing',
+    cards: [
+      {
+        title: 'Array operations',
+      }
+    ]
+  },
+  {
+    title: 'Done',
+    cards: [
+      {
+        title: 'Create a sample page',
+      },
+      {
+        title: 'Launch demo site'
+      }
+    ]
+  },
+];
+
+const client = new yorkie.Client('http://localhost:8080');
+const doc = new yorkie.Document('vuejs-kanban');
+
+export default {
+  data() {
+    return {
+      lists: [],
+      title: '',
+      opened: null,
+    };
+  },
+  created() {
+    this.fetchDoc();
+  },
+  watch: {
+    opened(index) {
+      this.$nextTick(function () {
+        if (index === 0) {
+          // Open add list form
+          this.$refs['addListForm'].querySelector('input').focus();
+        } else {
+          // Or open add card form
+          this.$refs['addCardForm'][index - 1].querySelector('input').focus();
+        }
+      });
+    },
+  },
+  methods: {
+    async fetchDoc() {
+      await client.activate();
+      await client.attach(doc);
+
+
+      doc.update((root) => {
+        if (!root.lists) {
+          root.lists = defaultLists;
+        }
+      }, 'create default list if not exists');
+
+      doc.subscribe((event) => {
+        this.lists = doc.getRoot().lists;
+      });
+      await client.sync();
+
+      this.lists = doc.getRoot().lists;
+    },
+
+    isOpened(index) {
+      return this.opened === index;
+    },
+
+    openForm(index) {
+      this.opened = index;
+    },
+
+    closeForm() {
+      this.opened = null;
+    },
+
+    addCard(list) {
+      if (this.title === '') return;
+
+      doc.update((root) => {
+        root.lists.getElementByID(list.getID()).cards.push({
+          title: this.title,
+        });
+        this.title = '';
+      }, `add new card by ${client.getID()}`);
+    },
+
+    deleteCard(list, card) {
+      doc.update((root) => {
+        root.lists.getElementByID(list.getID()).cards.deleteByID(card.getID());
+      }, `delete a card by ${client.getID()}`);
+    },
+
+    addList() {
+      if (this.title === '') return;
+
+      doc.update((root) => {
+        root.lists.push({
+          title: this.title,
+          cards: [],
+        });
+        this.title = '';
+      }, `add new list by ${client.getID()}`);
+    },
+
+    deleteList(list) {
+      doc.update((root) => {
+        root.lists.deleteByID(list.getID());
+      }, `delete a list by ${client.getID()}`);
+    },
+  },
+}
+</script>
+
+<template>
+  <div v-cloak class="list" v-for="(list, index) in lists">
+    <span class="delete" v-on:click="deleteList(list)">❌</span>
+    <div class="title">{{ list.title }}</div>
+    <div class="card" v-for="card in list.cards">
+      <span class="delete" v-on:click="deleteCard(list, card)">❌</span>
+      {{ card.title }}
+    </div>
+    <div class="add-card" ref="addCardForm">
+      <div v-if="isOpened(index + 1)" class="add-form">
+        <input type="text" placeholder="Enter card title" v-model="title" v-on:keyup.enter="addCard(list)"
+          v-on:keyup.esc="closeForm()">
+        <div class="buttons">
+          <input type="button" value="Add" v-on:click="addCard(list)">
+          <input type="button" value="Close" class="pull-right" v-on:click="closeForm()">
+        </div>
+      </div>
+      <div v-else class="add-card-opener" v-on:click="openForm(index + 1)">Add another card</div>
+    </div>
+  </div>
+  <div class="add-list" ref="addListForm">
+    <div v-if="isOpened(0)" class="add-form">
+      <input type="text" placeholder="Enter list title" v-model="title" v-on:keyup.enter="addList()"
+        v-on:keyup.esc="closeForm()">
+      <div class="buttons">
+        <input type="button" value="Add" v-on:click="addList()">
+        <input type="button" value="Close" class="pull-right" v-on:click="closeForm()">
+      </div>
+    </div>
+    <div v-else class="add-list-opener" v-on:click="openForm(0)">Add another list</div>
+  </div>
+</template>
