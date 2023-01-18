e0864e81b081a86bf0106c36218dbc7632bf8015
Yourim Cha <81357083+chacha912@users.noreply.github.com>
2023-01-18 11:36:40 +0900

    Add vanilla-quill example (#435)

---
+++ examples/vanilla-quill/src/utils.ts
@@ -0,0 +1,29 @@
+/* eslint-disable jsdoc/require-jsdoc */
+import { Document, Indexable, RichText } from 'yorkie-js-sdk';
+
+// function to display peers
+export function displayPeers(
+  elem: HTMLElement,
+  peers: Record<string, Indexable>,
+  clientID: string,
+) {
+  const usernames = [];
+
+  for (const [id, presence] of Object.entries(peers)) {
+    usernames.push(
+      id === clientID ? `<b>${presence.username}</b>` : presence.username,
+    );
+  }
+
+  elem.innerHTML = JSON.stringify(usernames);
+}
+
+// function to display document content
+export function displayLog<T extends { content: RichText }>(
+  elem: HTMLElement,
+  textElem: HTMLElement,
+  doc: Document<T>,
+) {
+  elem.innerText = doc.toJSON();
+  textElem.innerText = doc.getRoot().content.getStructureAsString();
+}
