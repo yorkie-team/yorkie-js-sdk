141f7b7120b91925366a124a2ef18e1cb5505b80
Hyein Kim <cozitive.dev@gmail.com>
2023-02-01 15:24:06 +0900

    Update version to v0.3.0 in examples

--- public/util.js
+++ public/util.js
@@ -8,23 +8,28 @@ const network = {
     network.isOnline = true;
     elem.innerHTML = '<span class="green"> </span>';
   },
-  statusListener: elem => {
+  statusListener: (elem) => {
     return (event) => {
-      if (network.isOnline && (
-        event.type == 'status-changed' && event.value == 'deactivated' ||
-        event.type == 'stream-connection-status-changed' && event.value == 'disconnected' ||
-        event.type == 'document-sync-result' && event.value == 'sync-failed'
-      )) {
+      if (
+        network.isOnline &&
+        ((event.type == 'status-changed' && event.value == 'deactivated') ||
+          (event.type == 'stream-connection-status-changed' &&
+            event.value == 'disconnected') ||
+          (event.type == 'document-sync-result' &&
+            event.value == 'sync-failed'))
+      ) {
         network.showOffline(elem);
-      } else if (!network.isOnline && (
-        event.type == 'status-changed' && event.value == 'activated' ||
-        event.type == 'stream-connection-status-changed' && event.value == 'connected' ||
-        event.type == 'document-sync-result' && event.value == 'synced' ||
-        event.type == 'peers-changed' ||
-        event.type == 'documents-changed'
-      )) {
+      } else if (
+        !network.isOnline &&
+        ((event.type == 'status-changed' && event.value == 'activated') ||
+          (event.type == 'stream-connection-status-changed' &&
+            event.value == 'connected') ||
+          (event.type == 'document-sync-result' && event.value == 'synced') ||
+          event.type == 'peers-changed' ||
+          event.type == 'documents-changed')
+      ) {
         network.showOnline(elem);
       }
-    }
+    };
   },
-}
+};
