453d5aaeebe289f2c41fe2fd08a937af2d1477b5
JOOHOJANG <jayjangjoo@gmail.com>
2023-06-13 20:54:44 +0900

    Add TC

--- test/unit/document/crdt/tree_test.ts
+++ test/unit/document/crdt/tree_test.ts
@@ -586,6 +586,42 @@ describe('CRDTTree', function () {
       /*html*/ `<root><p>ab</p><b>cd</b><p>ef</p></root>`,
     );
   });
+
+  it('Can edit different levels', function () {
+    const tree = new CRDTTree(
+      new CRDTTreeNode(issuePos(), 'root'),
+      issueTime(),
+    );
+    tree.editByIndex([0, 0], new CRDTTreeNode(issuePos(), 'p'), issueTime());
+    tree.editByIndex([1, 1], new CRDTTreeNode(issuePos(), 'b'), issueTime());
+    tree.editByIndex([2, 2], new CRDTTreeNode(issuePos(), 'i'), issueTime());
+    tree.editByIndex(
+      [3, 3],
+      new CRDTTreeNode(issuePos(), 'text', 'ab'),
+      issueTime(),
+    );
+    //     0  1  2   3 4 5    6   7   8
+    //<root><p><b><i> a b </i></b></p></root>
+    assert.deepEqual(
+      tree.toXML(),
+      /*html*/ `<root><p><b><i>ab</i></b></p></root>`,
+    );
+
+    let [from, to] = tree.pathToPosRange([0]);
+    let fromIdx = tree.toIndex(from);
+    let toIdx = tree.toIndex(to);
+    assert.deepEqual([fromIdx, toIdx], [7, 8]);
+
+    [from, to] = tree.pathToPosRange([0, 0]);
+    fromIdx = tree.toIndex(from);
+    toIdx = tree.toIndex(to);
+    assert.deepEqual([fromIdx, toIdx], [6, 7]);
+
+    [from, to] = tree.pathToPosRange([0, 0, 0]);
+    fromIdx = tree.toIndex(from);
+    toIdx = tree.toIndex(to);
+    assert.deepEqual([fromIdx, toIdx], [5, 6]);
+  });
 });
 
 describe.skip('Tree.split', function () {
