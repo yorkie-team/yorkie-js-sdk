a047391b93ad4f37d30197b7fc4d80aceaad093e
Yourim Cha <yourim.cha@navercorp.com>
2023-08-02 18:12:07 +0900

    Add test for pause and resume

--- test/integration/presence_test.ts
+++ test/integration/presence_test.ts
@@ -221,6 +221,62 @@ describe('Presence', function () {
       cursor: { x: 1, y: 1 },
     });
   });
+
+  it(`Should return only online clients`, async function () {
+    const c1 = new yorkie.Client(testRPCAddr);
+    const c2 = new yorkie.Client(testRPCAddr);
+    const c3 = new yorkie.Client(testRPCAddr);
+    await c1.activate();
+    await c2.activate();
+    await c3.activate();
+    const c1ID = c1.getID()!;
+    const c2ID = c2.getID()!;
+    const c3ID = c3.getID()!;
+
+    const docKey = toDocKey(`${this.test!.title}-${new Date().getTime()}`);
+    type PresenceType = { name: string; cursor: { x: number; y: number } };
+    const doc1 = new yorkie.Document<{}, PresenceType>(docKey);
+    await c1.attach(doc1, {
+      initialPresence: { name: 'a1', cursor: { x: 0, y: 0 } },
+    });
+    const stub1 = sinon.stub();
+    const unsub1 = doc1.subscribe('presence', stub1);
+
+    // 01. c2 attaches doc in realtime sync, and c3 attached doc in manual sync.
+    const doc2 = new yorkie.Document<{}, PresenceType>(docKey);
+    await c2.attach(doc2, {
+      initialPresence: { name: 'b1', cursor: { x: 0, y: 0 } },
+    });
+    const doc3 = new yorkie.Document<{}, PresenceType>(docKey);
+    await c3.attach(doc3, {
+      initialPresence: { name: 'c1', cursor: { x: 0, y: 0 } },
+      isRealtimeSync: false,
+    });
+    await waitStubCallCount(stub1, 1); // c2 watched
+
+    assert.deepEqual(doc1.getPresences(), [
+      { clientID: c1ID, presence: doc1.getMyPresence() },
+      { clientID: c2ID, presence: doc2.getMyPresence() },
+    ]);
+    assert.deepEqual(doc1.getPresence(c3ID), undefined);
+
+    // 02. c2 pauses the document (in manual sync), c3 resumes the document (in realtime sync).
+    await c2.pause(doc2);
+    await waitStubCallCount(stub1, 2); // c2 unwatched
+    await c3.resume(doc3);
+    await waitStubCallCount(stub1, 3); // c3 watched
+
+    assert.deepEqual(doc1.getPresences(), [
+      { clientID: c1ID, presence: doc1.getMyPresence() },
+      { clientID: c3ID, presence: doc3.getMyPresence() },
+    ]);
+    assert.deepEqual(doc1.getPresence(c2ID), undefined);
+
+    unsub1();
+    await c1.deactivate();
+    await c2.deactivate();
+    await c3.deactivate();
+  });
 });
 
 describe(`Document.Subscribe('presence')`, function () {
@@ -353,4 +409,154 @@ describe(`Document.Subscribe('presence')`, function () {
 
     unsub1();
   });
+
+  it(`Can receive presence-related event only when using realtime sync`, async function () {
+    const c1 = new yorkie.Client(testRPCAddr);
+    const c2 = new yorkie.Client(testRPCAddr);
+    const c3 = new yorkie.Client(testRPCAddr);
+    await c1.activate();
+    await c2.activate();
+    await c3.activate();
+    const c2ID = c2.getID()!;
+    const c3ID = c3.getID()!;
+
+    const docKey = toDocKey(`${this.test!.title}-${new Date().getTime()}`);
+    type PresenceType = { name: string; cursor: { x: number; y: number } };
+    const doc1 = new yorkie.Document<{}, PresenceType>(docKey);
+    await c1.attach(doc1, {
+      initialPresence: { name: 'a1', cursor: { x: 0, y: 0 } },
+    });
+    const stub1 = sinon.stub();
+    const unsub1 = doc1.subscribe('presence', stub1);
+
+    // 01. c2 attaches doc in realtime sync, and c3 attached doc in manual sync.
+    //     c1 receives the watched event from c2.
+    const doc2 = new yorkie.Document<{}, PresenceType>(docKey);
+    await c2.attach(doc2, {
+      initialPresence: { name: 'b1', cursor: { x: 0, y: 0 } },
+    });
+    const doc3 = new yorkie.Document<{}, PresenceType>(docKey);
+    await c3.attach(doc3, {
+      initialPresence: { name: 'c1', cursor: { x: 0, y: 0 } },
+      isRealtimeSync: false,
+    });
+    await waitStubCallCount(stub1, 1); // c2 watched
+
+    // 02. c2 and c3 update the presence.
+    //     c1 receives the presence-changed event from c2.
+    doc2.update((_, presence) => {
+      presence.set({ name: 'b2' });
+    });
+    doc3.update((_, presence) => {
+      presence.set({ name: 'c2' });
+    });
+    await waitStubCallCount(stub1, 2); // c2 presence-changed
+
+    // 03. c2 pauses the document (in manual sync), c3 resumes the document (in realtime sync).
+    //     c1 receives an unwatched event from c2 and a watched event from c3.
+    await c2.pause(doc2);
+    await waitStubCallCount(stub1, 3); // c2 unwatched
+    await c3.resume(doc3);
+    await waitStubCallCount(stub1, 5); // c3 watched, c3 presence-changed
+
+    // 04. c2 and c3 update the presence.
+    //     c1 receives the presence-changed event from c3.
+    doc2.update((_, presence) => {
+      presence.set({ name: 'b3' });
+    });
+    doc3.update((_, presence) => {
+      presence.set({ name: 'c3' });
+    });
+    await waitStubCallCount(stub1, 6); // c3 presence-changed
+
+    // 05. c3 pauses the document (in manual sync),
+    //     c1 receives an unwatched event from c3.
+    await c3.pause(doc3);
+    await waitStubCallCount(stub1, 7); // c3 unwatched
+
+    // 06. c2 performs manual sync and then resumes(switches to realtime sync).
+    //     After applying all changes, only the watched event is triggered.
+    await c2.sync();
+    await c2.resume(doc2);
+    await waitStubCallCount(stub1, 8); // c2 watched
+
+    assert.deepEqual(stub1.args, [
+      [
+        {
+          type: DocEventType.Watched,
+          value: {
+            clientID: c2ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'b1' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.PresenceChanged,
+          value: {
+            clientID: c2ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'b2' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.Unwatched,
+          value: {
+            clientID: c2ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'b2' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.Watched,
+          value: {
+            clientID: c3ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'c1' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.PresenceChanged,
+          value: {
+            clientID: c3ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'c2' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.PresenceChanged,
+          value: {
+            clientID: c3ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'c3' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.Unwatched,
+          value: {
+            clientID: c3ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'c3' },
+          },
+        },
+      ],
+      [
+        {
+          type: DocEventType.Watched,
+          value: {
+            clientID: c2ID,
+            presence: { cursor: { x: 0, y: 0 }, name: 'b3' },
+          },
+        },
+      ],
+    ]);
+    unsub1();
+    await c1.deactivate();
+    await c2.deactivate();
+    await c3.deactivate();
+  }).timeout(10000);
 });
