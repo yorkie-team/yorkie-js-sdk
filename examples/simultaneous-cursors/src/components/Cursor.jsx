65ef4d5f39a6c47e72e84435777de146c6da3821
YoonKiJin <ykjin5715@gmail.com>
2023-08-03 16:58:18 +0900

    Tweaking Cursor.jsx

--- examples/simultaneous-cursors/src/components/Cursor.jsx
+++ examples/simultaneous-cursors/src/components/Cursor.jsx
@@ -4,52 +4,22 @@ import FullAnimation from './FullAnimation';
 const Cursor = ({ selectedCursorShape, x, y, pointerDown }) => {
   return (
     <>
-      {selectedCursorShape === 'heart' && (
-        <>
-          <img
-            src={'src/assets/icons/icon_heart.svg'}
-            className="heart-cursor"
-            style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
-          />
-          <FullAnimation
-            pointerDown={pointerDown}
-            xPos={x}
-            yPos={y}
-            selectedCursorShape={selectedCursorShape}
-          />
-        </>
-      )}
-      {selectedCursorShape === 'thumbs' && (
-        <>
-          <img
-            src={'src/assets/icons/icon_thumbs.svg'}
-            className="thumbs-cursor"
-            style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
-          />
-          <FullAnimation
-            pointerDown={pointerDown}
-            xPos={x}
-            yPos={y}
-            selectedCursorShape={selectedCursorShape}
-          />
-        </>
-      )}
-      {selectedCursorShape === 'pen' && (
-        <>
-          <img
-            src={'src/assets/icons/icon_pen.svg'}
-            className="pen-cursor"
-            style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
-          />
-          {pointerDown && <PenCursor xPos={x} yPos={y} />}
-        </>
-      )}
-      {selectedCursorShape === 'cursor' && (
-        <img
-          src={'src/assets/icons/icon_cursor.svg'}
-          className="cursor-cursor"
-          style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
+      <img
+        src={`src/assets/icons/icon_${selectedCursorShape}.svg`}
+        className={`${selectedCursorShape}-cursor`}
+        style={{ transform: `translate3d(${x}px, ${y}px, 0)` }}
+      />
+      {(selectedCursorShape === 'heart' ||
+        selectedCursorShape === 'thumbs') && (
+        <FullAnimation
+          pointerDown={pointerDown}
+          xPos={x}
+          yPos={y}
+          selectedCursorShape={selectedCursorShape}
         />
+      )}
+      {selectedCursorShape === 'pen' && pointerDown && (
+        <PenCursor xPos={x} yPos={y} />
       )}
     </>
   );
