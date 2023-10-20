03d0286257051400606f38162bbd5159440044be
Youngteac Hong <susukang98@gmail.com>
2023-10-20 10:51:51 +0900

    Update nextjs-scheduler to export static files

--- examples/nextjs-scheduler/next.config.js
+++ examples/nextjs-scheduler/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
