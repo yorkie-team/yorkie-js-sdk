3415e28404f818f52e6ab2719a612cd2bd3f072b
blurfx <iam@xo.dev>
2023-11-19 00:33:21 +0900

    fix: approach 1 - disable swcMinify

--- examples/nextjs-scheduler/next.config.js
+++ examples/nextjs-scheduler/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   distDir: 'dist',
   basePath: '/yorkie-js-sdk/examples/nextjs-scheduler',
   assetPrefix: '/yorkie-js-sdk/examples/nextjs-scheduler/',
+  swcMinify: false, // See https://github.com/vercel/next.js/issues/38436
 };
 
 module.exports = nextConfig;
