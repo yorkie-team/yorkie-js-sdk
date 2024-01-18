39d99d7dcfa542d70d6e39d826c1782a263a9554
Youngteac Hong <susukang98@gmail.com>
2024-01-18 20:04:02 +0900

    Add copyright

--- tools/devtools/src/protocol.ts
+++ tools/devtools/src/protocol.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // TODO(chacha912): This code is a copy from src/devtools/protocol.ts.
 // It is intended to be used by importing it from yorkie-js-sdk when
 // it is structured as a monorepo.
