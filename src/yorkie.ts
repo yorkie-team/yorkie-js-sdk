db34be7a1066f0762f502db877ef896888ca6c69
Yourim Cha <yourim.cha@navercorp.com>
2024-03-04 10:02:53 +0900

    Export Devtools

--- src/yorkie.ts
+++ src/yorkie.ts
@@ -20,7 +20,7 @@ import { Text } from '@yorkie-js-sdk/src/document/json/text';
 import { Tree } from '@yorkie-js-sdk/src/document/json/tree';
 import { Counter } from '@yorkie-js-sdk/src/document/json/counter';
 import { CounterType } from '@yorkie-js-sdk/src/document/crdt/counter';
-import type * as Devtools from '@yorkie-js-sdk/src/devtools/types';
+import * as Devtools from '@yorkie-js-sdk/src/devtools/types';
 import Long from 'long';
 
 export {
@@ -121,7 +121,7 @@ export {
   FullPanelToSDKMessage,
   FullSDKToPanelMessage,
 } from '@yorkie-js-sdk/src/devtools/protocol';
-export type { Devtools };
+export { Devtools };
 
 /**
  * The top-level yorkie namespace with additional properties.
