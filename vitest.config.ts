4f3a91ab008435426d612519840c1588edf1a8fb
Youngteac Hong <susukang98@gmail.com>
2024-07-09 15:30:37 +0900

    Remove custom-jsdom

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
     onConsoleLog() {
       return false;
     },
-    environment: 'custom-jsdom',
     globals: true,
     testTimeout: isCI ? 5000 : Infinity,
     benchmark: {
