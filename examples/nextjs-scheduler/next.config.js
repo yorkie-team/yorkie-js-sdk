b1d263c6c57ba0096008b5b0427544080398fa4f
Paik <gwbaik9717@icloud.com>
2024-08-09 08:22:10 +0900

    Update TypeScript and Vite version

--- examples/nextjs-scheduler/next.config.js
+++ examples/nextjs-scheduler/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   distDir: 'dist',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
--- examples/profile-stack/vite.config.js
+++ examples/profile-stack/vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
+  resolve: {
+    alias: [
+      {
+        find: '@yorkie-js-sdk/src',
+        replacement: path.resolve(__dirname, '../../packages/sdk/src'),
+      },
+    ],
+  },
 });
