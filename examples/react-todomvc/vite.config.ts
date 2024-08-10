1091704c27deded7af9fc36f35fe964ebaab07ce
Paik <gwbaik9717@icloud.com>
2024-08-10 10:17:18 +0900

    Update tsconfig.json and vite.config.ts in examples

--- examples/react-todomvc/vite.config.ts
+++ examples/react-todomvc/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,4 +6,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '',
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@yorkie-js-sdk/src',
+        replacement: path.resolve(__dirname, '../../packages/sdk/src'),
+      },
+    ],
+  },
 });
