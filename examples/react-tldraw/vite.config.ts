b753af098cceb3abf67b6f4ce1e82d876bfc9f27
Paik <gwbaik9717@icloud.com>
2024-08-11 11:07:32 +0900

    Resolve issue of peer dependcies of plugin-react and tldraw

--- examples/react-tldraw/vite.config.ts
+++ examples/react-tldraw/vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: [
       {
