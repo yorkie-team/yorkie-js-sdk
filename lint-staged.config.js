8070eb81258f1b91b535c88a93f0f863aa99f8f9
witch-factory <soakdma37@gmail.com>
2025-08-02 17:48:04 +0900

    remove unused import in lint-staged config file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,3 @@
-const { ESLint } = require('eslint');
-const { execSync } = require('child_process');
-const path = require('path');
-
 module.exports = {
   '**/*.{ts,tsx}': 'pnpm exec eslint --fix --max-warnings=0 --no-warn-ignored',
 };
