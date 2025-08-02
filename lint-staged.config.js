54d5e00458bc46b0a08571808438db6402c40ecb
witch-factory <soakdma37@gmail.com>
2025-08-02 17:24:37 +0900

    remove unused code in lint-staged config file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,21 +2,6 @@ const { ESLint } = require('eslint');
 const { execSync } = require('child_process');
 const path = require('path');
 
-const removeIgnoredFiles = async (files) => {
-  const eslintIgnorePath = path.resolve('.eslintignore'); // Pointing to the root .eslintignore
-  const eslint = new ESLint({ ignorePath: eslintIgnorePath });
-
-  const isIgnored = await Promise.all(
-    files.map(async (file) => {
-      const ignored = await eslint.isPathIgnored(file);
-      return ignored;
-    }),
-  );
-
-  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
-  return filteredFiles;
-};
-
 module.exports = {
   '**/*.{ts,tsx}': 'pnpm exec eslint --fix --max-warnings=0 --no-warn-ignored',
 };
